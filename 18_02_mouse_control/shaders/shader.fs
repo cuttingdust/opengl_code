4460a235e299e450a06105cdf442e27bf1d2eb64
cuttingDust_47 <18862963156@163.com>
2022-12-17 00:29:58 +0800

    鼠标右击控制物体

---
+++ 18_02_mouse_control/shaders/shader.fs
@@ -0,0 +1,20 @@
+#version 330 core
+out vec4 FragColor;
+float near = 0.1;
+float far = 100.0;
+float LinearizeDepth(float depth) {
+float z_ndc = depth * 2.0 - 1.0; // back to NDC
+return (2.0 * near * far) / (far + near - z_ndc  * (far - near));
+}
+
+uniform sampler2D texture1;
+in vec2 TexCoords;
+
+void main() {
+    FragColor = texture(texture1, TexCoords);
+    
+    //FragColor = vec4(vec3(gl_FragCoord.z), 1.0f);
+
+    //float depth = LinearizeDepth(gl_FragCoord.z) / far; // divide by far for demonstration
+    //FragColor = vec4(vec3(depth), 1.0);
+}
