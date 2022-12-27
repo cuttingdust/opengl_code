541d7a441ebf8b91b8227dda96033efb52cfeb21
cuttingDust_47 <18862963156@163.com>
2022-12-28 01:47:36 +0800

    增加实例化技术-小行星环实例

--- 27_03_instancing/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/handabao/MyFile/code/opengl/27_03_instancing
-# Build directory: /Users/handabao/MyFile/code/opengl/27_03_instancing/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
