8a332f41b7771a3ea7fda5f3ac86cddae59c6dda
cuttingDust_47 <18862963156@163.com>
2023-01-10 01:30:57 +0800

    删除多余文件

--- 41_02_PBRLighting/build/HelloGL.build/Debug/HelloGL.build/Objects-normal/arm64/glad.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /Users/handabao/MyFile/code/opengl/41_02_PBRLighting/glad/src/glad.c \
-  /Users/handabao/MyFile/code/opengl/41_02_PBRLighting/glad/include/glad/glad.h \
-  /Users/handabao/MyFile/code/opengl/41_02_PBRLighting/glad/include/KHR/khrplatform.h
