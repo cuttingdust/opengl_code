919d36e978ea46cfb5422de08960c3bf08bc1b61
cuttingDust_47 <18862963156@163.com>
2023-07-20 01:02:21 +0800

    删除多余的数据

--- 01_HelloGL/build_Debug/CMakeFiles/3.24.20220910-g9790821-dirty/CompilerIdC/CompilerIdC.build/Debug/CompilerIdC.build/Objects-normal/arm64/CMakeCCompilerId.d
+++
@@ -1,2 +0,0 @@
-dependencies: \
-  /Users/handabao/MyFile/code/opengl/01_HelloGL/build_Debug/CMakeFiles/3.24.20220910-g9790821-dirty/CompilerIdC/CMakeCCompilerId.c
--- 01_HelloGL/build_Debug/HelloGL.build/Debug/HelloGL.build/Objects-normal/arm64/glad.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /Users/handabao/MyFile/code/opengl/01_HelloGL/glad/src/glad.c \
-  /Users/handabao/MyFile/code/opengl/01_HelloGL/glad/include/glad/glad.h \
-  /Users/handabao/MyFile/code/opengl/01_HelloGL/glad/include/KHR/khrplatform.h
