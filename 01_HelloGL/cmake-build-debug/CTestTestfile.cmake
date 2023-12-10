34d94f55e5a6b74871399b20e3e16d71af1ee563
cuttingDust_47 <18862963156@163.com>
2023-12-10 22:43:52 +0800

    删除多余的数据

--- 01_HelloGL/cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/handabao/MyFile/code/opengl/01_HelloGL
-# Build directory: /Users/handabao/MyFile/code/opengl/01_HelloGL/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
