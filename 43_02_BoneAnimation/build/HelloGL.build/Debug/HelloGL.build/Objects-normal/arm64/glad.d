02096d5c9a72d09b938f8e7eb63f4012bc5cc45f
cuttingDust_47 <18862963156@163.com>
2023-12-11 00:15:39 +0800

    删除多余的数据

--- 43_02_BoneAnimation/build/HelloGL.build/Debug/HelloGL.build/Objects-normal/arm64/glad.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /Users/handabao/MyFile/code/opengl/43_02_BoneAnimation/glad/src/glad.c \
-  /Users/handabao/MyFile/code/opengl/43_02_BoneAnimation/glad/include/glad/glad.h \
-  /Users/handabao/MyFile/code/opengl/43_02_BoneAnimation/glad/include/KHR/khrplatform.h
